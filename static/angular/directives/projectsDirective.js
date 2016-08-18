a225043cc15bbec047dc30d2e01abe911cf5ee4b
Rahat Dhande <rahatchd@gmail.com>
2016-08-18 02:35:13 -0700

    refactors projects page, makes tiles prettier

--- static/angular/directives/projectsDirective.js
+++ static/angular/directives/projectsDirective.js
@@ -4,7 +4,16 @@
             return {
                 restrict: "E",
                 templateUrl: "/static/angular/templates/projects.html",
-                controller: "projectsController"
+                controller: "projectsController",
+                link: function (scope, element) {
+                    var children = $(element).find('.profile-wrapper');
+                    for (var ii = 0; ii < children.length; ++ii) {
+                        $(children[ii]).stellar({
+                            horizontalScrolling: true,
+                            verticalScrolling: false
+                        })
+                    }
+                }
             }
         });
 })();
--- static/angular/directives/teamsDirective.js
+++ static/angular/directives/teamsDirective.js
@@ -4,7 +4,16 @@
             return {
                 restrict: "E",
                 templateUrl: "/static/angular/templates/teams.html",
-                controller: "teamsController"
+                controller: "teamsController",
+                link: function (scope, element) {
+                    var children = $(element).find('.profile-wrapper');
+                    for (var ii = 0; ii < children.length; ++ii) {
+                        $(children[ii]).stellar({
+                            horizontalScrolling: true,
+                            verticalScrolling: false
+                        })
+                    }
+                }
             }
         });
 })();
