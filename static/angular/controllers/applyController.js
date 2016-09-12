91d3048b24c0743c639f69666264762a9318da88
Rahat Dhande <rahatchd@gmail.com>
2016-09-12 10:23:53 -0700

    updates recruitment status

--- static/angular/controllers/applyController.js
+++ static/angular/controllers/applyController.js
@@ -1,6 +1,7 @@
 (function () {
     angular.module('app')
         .controller('applyController', function (DataFactory, $scope) {
+            $scope.activeRecruitment = false;
             $scope.recruitmentMessage = null;
             $scope.events = null;
 
@@ -8,8 +9,10 @@
                 .success(function (data) {
                     var status = data.status;
                     if (status.active) {
+                        $scope.activeRecruitment = true;
                         $scope.recruitmentMessage = status.activeMessage;
                     } else {
+                        $scope.activeRecruitment = false;
                         $scope.recruitmentMessage = status.inactiveMessage;
                     }
                 });
